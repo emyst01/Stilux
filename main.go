93a2b79f1e218602c9479a016dd77f8d2d2b7d9f
Stilo <81645252+emyst01@users.noreply.github.com>
2023-10-05 21:04:22 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"stilux/packages/variable"
 	"strconv"
 	"strings"
+
+	"github.com/Knetic/govaluate"
 )
 
 var StringVariables []variable.Str
@@ -17,23 +19,62 @@ var BooleanVariables []variable.Boolean
 
 func doAbsolutelyNothing(something string) {}
 
-func print(msg string) {
-	fmt.Println(msg)
-}
-
 func main() {
 	c, err := read.Read()
 	if err != nil {
 		print(err.Error())
 		return
 	}
-
 	instructions := strings.Split(c, "-|")
+	execute(instructions)
+}
+
+func execute(instructions []string) {
 
 	for i, instruction := range instructions {
 		if strings.HasPrefix(instruction, "out ") {
 			instruction = strings.TrimPrefix(instruction, "out ")
-			print(instruction)
+
+			if strings.Contains(instruction, "${") {
+				re := regexp.MustCompile(`\${([^}]+)}`)
+				instruction = re.ReplaceAllStringFunc(instruction, func(match string) string {
+					parts := strings.Split(strings.Trim(match, "${}"), "-")
+					if len(parts) < 2 {
+						return fmt.Sprintf("Error on instruction %d. You must specify the variable type\n", i+1)
+					}
+					variableType := parts[0]
+					variableName := parts[1]
+
+					switch variableType {
+					case "string":
+						if strVar, err := variable.FindStrByName(StringVariables, variableName); err == nil {
+							return strVar.Content
+						}
+					case "int":
+						if intVar, err := variable.FindIntegerByName(IntegerVariables, variableName); err == nil {
+							return strconv.Itoa(intVar.Content)
+						}
+					case "float1":
+						if float1Var, err := variable.FindFloat1ByName(Float1Variables, variableName); err == nil {
+							return fmt.Sprintf("%f", float1Var.Content)
+						}
+					case "float2":
+						if float2Var, err := variable.FindFloat2ByName(Float2Variables, variableName); err == nil {
+							return fmt.Sprintf("%f", float2Var.Content)
+						}
+					case "bool":
+						if boolVar, err := variable.FindBooleanByName(BooleanVariables, variableName); err == nil {
+							return strconv.FormatBool(boolVar.Content)
+						}
+					default:
+						return fmt.Sprintf("Unknown variable type: %s\n", variableType)
+					}
+
+					return fmt.Sprintf("\nVariable not found: %s-%s\n", variableType, variableName)
+				})
+			}
+
+			fmt.Println(instruction)
 		} else if strings.HasPrefix(instruction, "var ") {
 			instruction = strings.TrimPrefix(instruction, "var ")
 			re := regexp.MustCompile(`\s+`)
@@ -81,6 +122,60 @@ func main() {
 			default:
 				fmt.Printf("Error at instruction %d, This variable type doesn't exist %s: %s\n", i+1, args[0], args[1])
 			}
+		} else if strings.HasPrefix(instruction, "in ") {
+			instruction = strings.TrimPrefix(instruction, "in ")
+			instruction = strings.ReplaceAll(instruction, " ", "")
+			var userinput string
+			fmt.Scanln(&userinput)
+			newVar := variable.Str{Name: instruction, Content: userinput}
+			StringVariables = append(StringVariables, newVar)
+		} else if strings.HasPrefix(instruction, "if ") {
+			instruction = strings.TrimPrefix(instruction, "if ")
+			instruction = strings.ReplaceAll(instruction, "\n", "")
+			instruction = strings.ReplaceAll(instruction, "    ", "")
+			condition(instruction)
+		}
+	}
+}
+
+func condition(ifStatement string) {
+
+	ifStatementArr := strings.Split(ifStatement, ":")
+	blocks := strings.Split(ifStatementArr[1], "-")
+
+	if len(blocks) > 2 {
+		fmt.Println("The if statement has only one false statement")
+	} else if len(blocks) < 0 {
+		fmt.Println("You must specify what happens when the condition is satisfied")
+	}
+
+	trueStatement := strings.Split(blocks[0], ";")
+	falseStatement := strings.Split(blocks[1], ";")
+
+	expression, err := govaluate.NewEvaluableExpression(ifStatementArr[0])
+	if err != nil {
+		fmt.Println("Couldn't create the expression:", err)
+		return
+	}
+
+	parameters := make(map[string]interface{})
+
+	result, err := expression.Evaluate(parameters)
+
+	if err != nil {
+		fmt.Println("Error during expression evaluation:", err)
+		return
+	}
+
+	if len(blocks) == 1 {
+		if result.(bool) {
+			execute(trueStatement)
+		}
+	} else if len(blocks) == 2 {
+		if result.(bool) {
+			execute(trueStatement)
+		} else {
+			execute(falseStatement)
 		}
 	}
 }
